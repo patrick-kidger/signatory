9b4c950e023903021eb76e02116e2404f0e63f4d
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2019-09-27 20:21:32 +0100

    Tweak

--- .github/workflows_templates/from_template.py
+++ .github/workflows_templates/from_template.py
@@ -115,9 +115,11 @@ linux = "ubuntu-16.04",
 mac = "macOS-10.14",
 
 # Versions of Python
-py27 = '2.7',
-py35 = '3.5.0',
-py36 = '3.6.0',
+# Note that it's actually important to specify the patch number here as well for maximum compatability.
+# (e.g. 3.6.6 vs 3.6.9 does break things)
+py27 = '2.7.13',
+py35 = '3.5.4',
+py36 = '3.6.2',
 py37 = '3.7.0',
 
 # Run on repository_dispatch and precisely one other event
