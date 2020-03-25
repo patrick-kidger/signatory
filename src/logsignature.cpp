4e17a15985640bb6773ac8fc95c937fdf069f4ca
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2020-03-25 12:46:07 +0000

    Fixed bug with releasing GIL

--- src/logsignature.cpp
+++ src/logsignature.cpp
@@ -178,91 +178,100 @@ namespace signatory {
                                       bool scalar_term) {
         logsignature::detail::logsignature_checkargs(signature, input_channel_size, depth, stream, scalar_term);
 
-        py::gil_scoped_release release;
-
-        if (scalar_term) {
-            signature = signature.narrow(/*dim=*/channel_dim, /*start=*/1, /*length=*/signature.size(channel_dim) - 1);
-        }
-
-        // Don't need to track gradients when we have a custom backward
-        signature = signature.detach();
-
-        torch::TensorOptions opts = misc::make_opts(signature);
-        torch::Tensor reciprocals = misc::make_reciprocals(depth, opts);
-        int64_t output_stream_size = stream ? signature.size(stream_dim) : -1;
-
-        // and allocate memory for the logsignature
-        torch::Tensor logsignature = torch::empty_like(signature);
-        std::vector<torch::Tensor> signature_by_term;
-        std::vector<torch::Tensor> logsignature_by_term;
-        misc::slice_by_term(signature, signature_by_term, input_channel_size, depth);
-        misc::slice_by_term(logsignature, logsignature_by_term, input_channel_size, depth);
-
-        if (stream) {
-            std::vector<torch::Tensor> signature_by_term_at_stream;
-
-            // The if statement is for safety's sake... we haven't had issues with this one, but there have been other
-            // issues we've run into with OpenMP+GPU on other for loops.
-            // (even though presumably those threads are just scheduling work for the GPU to do... ?)
-            #pragma omp parallel for default(none) \
-                                     if(!signature.is_cuda()) \
-                                     shared(output_stream_size, logsignature_by_term, signature_by_term, reciprocals)
-            for (int64_t stream_index = 0;
-                 stream_index < output_stream_size;
-                 ++stream_index) {
-                std::vector<torch::Tensor> signature_by_term_at_stream;
-                std::vector<torch::Tensor> logsignature_by_term_at_stream;
-
-                misc::slice_at_stream(signature_by_term, signature_by_term_at_stream, stream_index);
-                misc::slice_at_stream(logsignature_by_term, logsignature_by_term_at_stream, stream_index);
-
-                ta_ops::log(logsignature_by_term_at_stream, signature_by_term_at_stream, reciprocals);
-            }
-        }
-        else {
-            ta_ops::log(logsignature_by_term, signature_by_term, reciprocals);
-        }
-
-        // Brackets and Words are the two possible compressed forms of the logsignature. So here we perform the
-        // compression.
+        // must finish using Python objects before we release the GIL
         if (lyndon_info_capsule.is_none()) {
             lyndon_info_capsule = make_lyndon_info(input_channel_size, depth, mode);
         }
         logsignature::detail::LyndonInfo* lyndon_info =
                 misc::unwrap_capsule<logsignature::detail::LyndonInfo>(lyndon_info_capsule);
-        if (mode == LogSignatureMode::Words) {
-            logsignature = logsignature::detail::compress(*lyndon_info->lyndon_words, logsignature);
-        }
-        else if (mode == LogSignatureMode::Brackets){
-            logsignature = logsignature::detail::compress(*lyndon_info->lyndon_words, logsignature);
-            // This is essentially solving a sparse linear system... and it's horrendously slow on a GPU.
-            // There may well be ways of speeding this up beyond what's done here, but the brackets mode is definitely
-            // the least favoured child out of the mode options we provide. (It's inherently a strange choice in machine
-            // learning anyway, when the words mode is available.)
-            auto device = logsignature.device();
-            logsignature = logsignature.cpu();
-            // Then apply the transforms. We rely on the triangularity property of the Lyndon basis for this to work.
-            #pragma omp parallel for default(none) \
-                                     shared(lyndon_info, logsignature) schedule(dynamic,1)
-            for (s_size_type transform_class_index = 0;
-                 transform_class_index < static_cast<s_size_type>(lyndon_info->transforms.size());
-                 ++transform_class_index) {
-                // Note that it is very important that this inner loop operate serially!
-                for (const auto& transform : lyndon_info->transforms[transform_class_index]) {
-                    int64_t source_index = std::get<0>(transform);
-                    int64_t target_index = std::get<1>(transform);
-                    int64_t coefficient = std::get<2>(transform);
-                    torch::Tensor source = logsignature.narrow(/*dim=*/channel_dim,
-                                                               /*start=*/source_index,
-                                                               /*length=*/1);
-                    torch::Tensor target = logsignature.narrow(/*dim=*/channel_dim,
-                                                               /*start=*/target_index,
-                                                               /*length=*/1);
-                    target.sub_(source, coefficient);
+
+        torch::Tensor logsignature;
+        {  // release GIL
+            py::gil_scoped_release release;
+
+            if (scalar_term) {
+                signature = signature.narrow(/*dim=*/channel_dim, /*start=*/1,
+                                             /*length=*/signature.size(channel_dim) - 1);
+            }
+
+            // Don't need to track gradients when we have a custom backward
+            signature = signature.detach();
+
+            torch::TensorOptions opts = misc::make_opts(signature);
+            torch::Tensor reciprocals = misc::make_reciprocals(depth, opts);
+            int64_t output_stream_size = stream ? signature.size(stream_dim) : -1;
+
+            // and allocate memory for the logsignature
+            logsignature = torch::empty_like(signature);
+            std::vector <torch::Tensor> signature_by_term;
+            std::vector <torch::Tensor> logsignature_by_term;
+            misc::slice_by_term(signature, signature_by_term, input_channel_size, depth);
+            misc::slice_by_term(logsignature, logsignature_by_term, input_channel_size, depth);
+
+            if (stream) {
+                std::vector <torch::Tensor> signature_by_term_at_stream;
+
+                // The if statement is for safety's sake... we haven't had issues with this one, but there have been
+                // other issues we've run into with OpenMP+GPU on other for loops.
+                // (even though presumably those threads are just scheduling work for the GPU to do... ?)
+                #pragma omp parallel for default(none) \
+                                     if(!signature.is_cuda()) \
+                                     shared(output_stream_size, logsignature_by_term, signature_by_term, reciprocals)
+                for (int64_t stream_index = 0;
+                     stream_index < output_stream_size;
+                     ++stream_index) {
+                    std::vector <torch::Tensor> signature_by_term_at_stream;
+                    std::vector <torch::Tensor> logsignature_by_term_at_stream;
+
+                    misc::slice_at_stream(signature_by_term, signature_by_term_at_stream, stream_index);
+                    misc::slice_at_stream(logsignature_by_term, logsignature_by_term_at_stream, stream_index);
+
+                    ta_ops::log(logsignature_by_term_at_stream, signature_by_term_at_stream, reciprocals);
                 }
             }
-            logsignature = logsignature.to(device);
-        }
+            else {
+                ta_ops::log(logsignature_by_term, signature_by_term, reciprocals);
+            }
+
+            // Brackets and Words are the two possible compressed forms of the logsignature. So here we perform the
+            // compression.
+            if (mode == LogSignatureMode::Words) {
+                logsignature = logsignature::detail::compress(*lyndon_info->lyndon_words, logsignature);
+            }
+            else if (mode == LogSignatureMode::Brackets) {
+                logsignature = logsignature::detail::compress(*lyndon_info->lyndon_words, logsignature);
+                // This is essentially solving a sparse linear system... and it's horrendously slow on a GPU.
+                // There may well be ways of speeding this up beyond what's done here, but the brackets mode is
+                // definitely the least favoured child out of the mode options we provide. (It's typically a strange
+                // choice in machine learning anyway, when the words mode is available.)
+                // iisignature does manage to provide this mode efficiently on the CPU by collecting together Lyndon
+                // anagrams and then using pseudoinverses, which is an approach that might well work efficiently on the
+                // GPU, so that is a possibility
+                auto device = logsignature.device();
+                logsignature = logsignature.cpu();
+                // Then apply the transforms. We rely on the triangularity property of the Lyndon basis for this to work.
+                #pragma omp parallel for default(none) \
+                                     shared(lyndon_info, logsignature) schedule(dynamic, 1)
+                for (s_size_type transform_class_index = 0;
+                     transform_class_index < static_cast<s_size_type>(lyndon_info->transforms.size());
+                     ++transform_class_index) {
+                    // Note that it is very important that this inner loop operate serially!
+                    for (const auto& transform : lyndon_info->transforms[transform_class_index]) {
+                        int64_t source_index = std::get<0>(transform);
+                        int64_t target_index = std::get<1>(transform);
+                        int64_t coefficient = std::get<2>(transform);
+                        torch::Tensor source = logsignature.narrow(/*dim=*/channel_dim,
+                                                                   /*start=*/source_index,
+                                                                   /*length=*/1);
+                        torch::Tensor target = logsignature.narrow(/*dim=*/channel_dim,
+                                                                   /*start=*/target_index,
+                                                                   /*length=*/1);
+                        target.sub_(source, coefficient);
+                    }
+                }
+                logsignature = logsignature.to(device);
+            }
+        }  // finish released GIL
 
         return std::tuple<torch::Tensor, py::object> {logsignature, lyndon_info_capsule};
     }
@@ -276,6 +285,10 @@ namespace signatory {
                                                      py::object lyndon_info_capsule,
                                                      bool scalar_term) {
 
+        // Must do this before releasing the GIL.
+        logsignature::detail::LyndonInfo* lyndon_info =
+                misc::unwrap_capsule<logsignature::detail::LyndonInfo>(lyndon_info_capsule);
+
         py::gil_scoped_release release;
 
         if (scalar_term) {
@@ -285,8 +298,6 @@ namespace signatory {
         grad_logsignature = grad_logsignature.detach();
         signature = signature.detach();
 
-        logsignature::detail::LyndonInfo* lyndon_info =
-                misc::unwrap_capsule<logsignature::detail::LyndonInfo>(lyndon_info_capsule);
         torch::TensorOptions opts = misc::make_opts(signature);
         torch::Tensor reciprocals = misc::make_reciprocals(depth, opts);
         int64_t output_stream_size = stream ? signature.size(stream_dim) : -1;
